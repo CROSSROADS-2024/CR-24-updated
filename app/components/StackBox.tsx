fd061b97421fbe88dd36fc6c8dc11f4414c0b0ae
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-03-17 11:46:49 +0530

    update: StackBox

--- app/components/StackBox.tsx
+++ app/components/StackBox.tsx
@@ -1,11 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
 function StackBox() {
   return (
-    <div className="relative group w-[60%] 0">
-      <div className="absolute top-0 h-[400px] w-full bg-[#CC3272]  border-black border-2"></div>
-      <div className="absolute top-0 h-[400px] w-full bg-[#F58D14] -translate-x-[10px] -translate-y-[10px] group-hover:translate-x-[0px] group-hover:translate-y-[0px]  border-black border-2 transition-all delay-75 duration-30"></div>
-      <div className="absolute top-0 h-[400px] w-full bg-[#CC3272] -translate-x-[20px] -translate-y-[20px] group-hover:translate-x-[0px]  group-hover:translate-y-[0px]  border-black border-2 transition-all delay-75 duration-30"></div>
+    <div className="0 group relative w-[60%]">
+      {/* Base */}
+      <div className="absolute top-0 h-[400px] w-full border-2  border-black bg-[#CC3272]"></div>
+      <div className="absolute top-0 h-[400px] w-full -translate-x-[10px] -translate-y-[10px] border-2 border-black  bg-[#F58D14] transition-all delay-75 group-hover:translate-x-[0px] group-hover:translate-y-[0px]"></div>
+      {/* Top */}
+      <div className="flex absolute top-0 h-[400px] w-full -translate-x-[20px] -translate-y-[20px] border-2  border-black  bg-[#CC3272] transition-all delay-75 group-hover:translate-x-[0px] group-hover:translate-y-[0px] pr-5">
+        
+        <div className="w-[100%]">
+          <Image src="/Tiger-Campus.png" alt='' width={1080} height={920} className="w-full h-full"/>
+        </div>
+        <div className="w-full flex flex-col justify-between items-end">
+          <Image src="/arrow-up-right.svg" alt="" width={80} height={80} />
+
+         <div className="p-5">
+         <h1 className=" heading text-[#F58D14] text-right">
+            CAMPUS <br />
+            AMBASSADOR
+          </h1>
+          <h2 className="text-xl text-right">
+          be the face of crossroads <br/> in your campus !
+          </h2>
+         </div>
+        </div>
+      </div>
     </div>
   );
 }
