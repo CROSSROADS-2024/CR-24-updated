b982f6c52071d9e2f7b38fc8306c3564c66ebaf3
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-03-17 12:13:55 +0530

    fix: CampusAmbassador responsive

--- app/components/CampusAmbassador.tsx
+++ app/components/CampusAmbassador.tsx
@@ -3,28 +3,33 @@ import React from "react";
 
 function StackBox() {
   return (
-    <div className="0 group relative w-[60%]">
+    <div className="group relative w-[90%] p-1 md:w-[60%]">
       {/* Base */}
-      <div className="absolute top-0 h-[400px] w-full border-2  border-black bg-[#CC3272]"></div>
-      <div className="absolute top-0 h-[400px] w-full -translate-x-[10px] -translate-y-[10px] border-2 border-black  bg-[#F58D14] transition-all delay-75 group-hover:translate-x-[0px] group-hover:translate-y-[0px]"></div>
+      <div className="absolute top-0 h-[250px] w-full border-2 border-black  bg-[#CC3272] md:h-[400px]"></div>
+      <div className="absolute top-0 h-[250px] w-full -translate-x-[10px] -translate-y-[10px] border-2 border-black bg-[#F58D14]  transition-all delay-75 group-hover:translate-x-[0px] group-hover:translate-y-[0px] md:h-[400px]"></div>
       {/* Top */}
-      <div className="flex absolute top-0 h-[400px] w-full -translate-x-[20px] -translate-y-[20px] border-2  border-black  bg-[#CC3272] transition-all delay-75 group-hover:translate-x-[0px] group-hover:translate-y-[0px] pr-5">
-        
-        <div className="w-[100%]">
-          <Image src="/Tiger-Campus.png" alt='' width={1080} height={920} className="w-full h-full"/>
+      <div className="absolute top-0 flex h-[250px] w-full -translate-x-[20px] -translate-y-[20px]  overflow-hidden border-2  border-black bg-[#CC3272]   transition-all  delay-75 group-hover:translate-x-[0px] group-hover:translate-y-[0px] md:h-[400px]">
+        <div className="relative flex w-full h-full">
+          <Image
+            src="/Tiger-Campus.png"
+            alt=""
+            width={1080}
+            height={920}
+            className="left-0 bottom-0 absolute object-contain  "
+          />
         </div>
-        <div className="w-full flex flex-col justify-between items-end">
-          <Image src="/arrow-up-right.svg" alt="" width={80} height={80} />
+        <div className="flex w-full flex-col items-end justify-between p-2 pl-0 md:p-5">
+          <Image src="/arrow.svg" alt="" width={40} height={40} />
 
-         <div className="p-5">
-         <h1 className=" heading text-[#F58D14] text-right">
-            CAMPUS <br />
-            AMBASSADOR
-          </h1>
-          <h2 className="text-xl text-right">
-          be the face of crossroads <br/> in your campus !
-          </h2>
-         </div>
+          <div className="p-2 md:p-5">
+            <h1 className=" eventheading text-right text-[#F58D14]">
+              CAMPUS <br />
+              AMBASSADOR
+            </h1>
+            <h2 className="text-right text-xs md:text-xl">
+              be the face of crossroads <br className="hidden md:flex" /> in your campus !
+            </h2>
+          </div>
         </div>
       </div>
     </div>
